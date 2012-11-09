915f7db5235a1671304d2732d70f161bd7ccf392
Anis Benyoub <anisbenyoub@ubuntu.(none)>
2012-11-09 07:50:03 -0800

    Other fixes

--- src/mothertask.cpp
+++ src/mothertask.cpp
@@ -7,54 +7,54 @@ using namespace std;
 
 void * remplir_carton_function()
 {
-  cout<<"remplir crarton"<<endl;
+  cout<<"\t\tTâche remplir carton lancée"<<endl;
   pthread_exit(0);
 }
 
 void * imprimer_function()
 {
-  cout<<"imprimer "<<endl;
+  cout<<"\t\tTâche imprimer lancée"<<endl;
   pthread_exit(0);
 } 
 void * remplir_palette_function()
 {
-  cout<<"remplir palette"<<endl;
+  cout<<"\t\tTâche remplir palette lancée"<<endl;
   pthread_exit(0);
 }
 void * stocker_palette_function()
 {
-  cout<<"stocker palette"<<endl;
+  cout<<"\t\tTâche stocker palette lancée"<<endl;
   pthread_exit(0);
 }
 void * destocker_palette_function()
 {
-  cout<<"destocker palette "<<endl;
+  cout<<"\t\tTâche destocker palette lancée"<<endl;
   pthread_exit(0);
 }
 void * controleur_function()
 {
-  cout<<"controleur "<<endl;
+  cout<<"\t\tTâche controleur lancée"<<endl;
   pthread_exit(0);
 }
 void * serveur_reception_function()
 {
-  cout<<"serveur reception"<<endl;
+  cout<<"\t\tTâche serveur reception lancée"<<endl;
   pthread_exit(0);
 }
 void * serveur_envoi_function()
 {
-  cout<<"serveur envoi"<<endl;
+  cout<<"\t\tTâche serveur envoi lancée"<<endl;
   pthread_exit(0);
 }
 void * gestion_series_function()
 {
-  cout<<"gestion series"<<endl;
+  cout<<"\t\tTâche gestion series lancée"<<endl;
   pthread_exit(0);
 }
 
 int main()
 {
-
+  cout<<"Phase d'initialisation"<<endl;
 
 // Initialisation des boites aux lettres
   Mailbox<Event*> balEvenements;
@@ -62,20 +62,20 @@ int main()
   Mailbox<Carton*> palette;
   Mailbox<Palette*> stockage;
 
-  cout<<"Boites crées"<<endl;
+  cout<<"\tBoites  aux lettre créées"<<endl;
 // Initialisation mémoires partagées
   
   SharedMemory infos;
   infos.mutex=PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_init(&infos.mutex,NULL);
-  cout<<"SHMEM crées"<<endl;
+  cout<<"\tMémoires partagées créeés"<<endl;
 // Mutex
 
   pthread_mutex_t mutexLog=PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_init(&mutexLog,NULL);
-  cout<<"mutex crées"<<endl;
+  cout<<"\tMutex crées"<<endl;
 // Creation du gestionnaire de Log
-  cout<<"Gestionnaire de Log crée"<<endl;
+  cout<<"\tGestionnaire de Log créé"<<endl;
 
 //Creation des threads
   pthread_t remplir_carton, imprimer, remplir_palette,stocker_palette,destocker_palette,controleur,serveur_reception,serveur_envoi,gestion_series;
@@ -91,19 +91,39 @@ int main()
   pthread_create (&serveur_envoi, NULL, (void *) serveur_envoi_function, (void *) NULL);
   pthread_create (&gestion_series, NULL, (void *) gestion_series_function, (void *) NULL);
 
-  cout<<"Threadcrées"<<endl;
-  pthread_join(remplir_carton, NULL);
-  pthread_join(imprimer, NULL);
-  pthread_join(remplir_palette, NULL);
-  pthread_join(stocker_palette, NULL);
-  pthread_join(destocker_palette, NULL);
-  pthread_join(controleur, NULL);
+  cout<<"\t\t\tTout les thread ont été créés"<<endl;
+
+
+  cout<<"Phase moteur"<<endl;
+
+  int a;
+  cin>>a;
+
+  cout<<endl<<endl<<endl;
+  cout<<"Phase de destruction"<<endl;
+
+
+  pthread_join(gestion_series, NULL);
   pthread_join(serveur_reception, NULL);
   pthread_join(serveur_envoi, NULL);
   pthread_join(serveur_reception, NULL);
-  pthread_join(gestion_series, NULL);
+  pthread_join(controleur, NULL);
+  pthread_join(destocker_palette, NULL);
+  pthread_join(stocker_palette, NULL);
+  pthread_join(remplir_palette, NULL);
+  pthread_join(imprimer, NULL);
+  pthread_join(remplir_carton, NULL);
+  cout<<"\tThread détruits"<<endl;
 
-  cout<<"Thread détruits"<<endl;
+
+  cout<<"\tGestionnaire de Log detruit"<<endl;
+
+  cout<<"\tMémoires partagées detruites"<<endl;
+
+  cout<<"\tBoites  aux lettre détruites"<<endl;
+
+  cout<<"\tFin de l'application"<<endl;
+
   return 0;
   
 }
