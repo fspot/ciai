d9215a700c69bb1a04a2a64519afd4ad83c6126f
Anis Benyoub <anisbenyoub@ubuntu.(none)>
2012-11-12 11:53:47 -0800

    adding mutex

---
+++ src/multithreadObjects/mutex.h
@@ -0,0 +1,22 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+
+#include "pthread.h"
+
+class Mutex
+{
+	public:
+		Mutex();
+		~Mutex();
+
+		pthread_mutex_t* getMutex();
+
+		void lock(); // prendre le mutex (peut être bloquant)
+		void unlock(); // rend le mutex.
+
+	private:
+		pthread_mutex_t mut;
+};
+
+
+#endif
