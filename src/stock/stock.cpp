6e5616488e11198e9f08a506f74e35546e4bdb59
Fred <fred.matigot@gmail.com>
2012-11-14 12:06:46 +0100

    continuation stock

--- src/stock/stock.cpp
+++ src/stock/stock.cpp
@@ -1,1 +1,52 @@
- 
+#include <iostream>
+
+#include "stock.h"
+#include "pthread.h"
+#include "../mailbox/mailbox.h"
+#include "../modeles/modeles.h"
+
+using namespace std; 
+
+
+void *thread_stock(void *argStock)
+{
+	cout << "Thread Stock launched" << endl;
+
+	// ==== unpack arguments
+	ArgStock *infos = (ArgStock*) argStock;
+	Mailbox<Palette>* balStockage = argStock->balStockage;
+	Mailbox<Event>* balEvenements = argStock->balEvenements;
+	sem_t* reprise = argStock->reprise;
+	pthread_cond_t* cv  = argStock->cv;
+	pthread_mutex_t* mutCv = argStock->mutCv;
+	SharedMemoryLots *shMemLots = argStock->shMemLots;
+	// stock ?
+
+	// ==== semaphore wait (synchronisation du lancement des threads moteurs)
+	// SEM_WAIT()
+
+	Palette p;
+	int lot = 0, pal = 0; // num lot actuel, num palette actuelle (au sein du lot)
+
+	while(lot < shMemLots->content->tot)
+		p = balStockage->Pull(); // opération qui peut être bloquante : réception d'une palette à stocker.
+
+		if (p.fin) // test cas spécial
+			break;
+
+		// "eventuellement" on peut verifier si la palette correspond..
+
+		// ==== phase de stockage
+		// prise du mutex du stock
+		// stock[nomLot]++; // stockage 
+		// rendu du mutex du stock
+
+		pal++;
+		if (pal == shMemLots->content->lots.palettes) {
+			lot++; // passage au lot suivant
+			pal = 0;
+		}
+	}
+
+	pthread_exit(0);
+}
