db09bf5c82c51cdd0e02fd58cb9dbfa74fd6d1ed
Vol Renaud <rvol@if219-18.insa-lyon.fr>
2012-11-14 09:16:37 +0100

    gestion file après imprimer avec Size Mailbox

--- src/tacheImprimer/imprimer.h
+++ src/tacheImprimer/imprimer.h
@@ -8,62 +8,62 @@ using namespace std;
 
 struct ArgImprimer
 {
-  Mailbox<Event> * eventBox;
-  Mailbox<Carton> * balImprimante;
-  Mailbox<Carton> * balPalette;
-  pthread_cond_t * varCond;
-  pthread_mutex_t * mutex;
+	Mailbox<Event> * eventBox;
+	Mailbox<Carton> * balImprimante;
+	Mailbox<Carton> * balPalette;
+	pthread_cond_t * varCond;
+	pthread_mutex_t * mutex;
 };
 
 int imprimer_thread(void * argsUnconverted)
 {
-  ArgImprimer * args= (ArgImprimer *)argsUnconverted;
-  cout<<"Tâche imprimer lancée"<<endl;
-  
-  bool panneImprimante = false;
-  Carton cartonImpression;
-  int nbCartonFile = 0;
+	ArgImprimer * args= (ArgImprimer *)argsUnconverted;
+	cout<<"Tâche imprimer lancée"<<endl;
+	
+	bool panneImprimante = false;
+	Carton cartonImpression;
+	
+	pthread_cond_t varCond;
+	pthread_mutex_t mutex;
 
-  pthread_cond_t varCond;
-  pthread_mutex_t mutex;
-  for (;;) 
-    {
-      cartonImpression = args->balImprimante->Pull();
-      // Verification panne imprimante
-      // panneImprimante = 
+	for (;;) {
 
-      if (!panneImprimante) {
+		cartonImpression = args->balImprimante->Pull();
 
-	if (nbCartonFile < 10) {
+		// Verification panne imprimante
+		// panneImprimante = 
 
-	  args->balPalette->Push(cartonImpression,0);
-	  nbCartonFile++; // décrémentation à faire du coté de la tache remplir_palette
+		if (!panneImprimante) {
 
+			if (args->balPalette->Size() < 10) {
+
+				args->balPalette->Push(cartonImpression,0);
+
+			}
+			else {
+
+				// Depot message erreur dans la bal enventbox
+				Event msgErreurFileAttente(FILEATTPLEINE);
+				args->eventBox->Push( msgErreurFileAttente,0 );
+				
+				// Mise en attente d'un signal envoye par le controleur pour reprendre
+				pthread_mutex_lock( args->mutex );
+				pthread_cond_wait( args->varCond, args->mutex );
+				pthread_mutex_unlock( args->mutex );
+
+			}
+
+		}
+		else {
+
+			// Depot message erreur dans la bal eventbox
+			Event msgErreurImp(PANNEIMPRIM);
+			args->eventBox->Push( msgErreurImp ,0);
+		
+			// Mise en attente d'un signal envoye par le controleur pour reprendre
+			pthread_mutex_lock( args->mutex );
+			pthread_cond_wait( args->varCond, args->mutex );
+			pthread_mutex_unlock( args->mutex );
+		}
 	}
-	else {
-
-	  // Depot message erreur dans la bal balEvenenements
-	  Event msgErreurFileAttente(FILEATTPLEINE);
-	  args->eventBox->Push( msgErreurFileAttente,0 );
-
-	  // Mise en attente d'un signal envoye par le controleur pour reprendre
-	  pthread_mutex_lock( args->mutex );
-	  pthread_cond_wait( args->varCond, args->mutex );
-	  pthread_mutex_unlock( args->mutex );
-
-	}
-
-      }
-      else {
-
-	// Depot message erreur dans la bal balEvenenements
-	Event msgErreurImp(PANNEIMPRIM);
-	args->eventBox->Push( msgErreurImp ,0);
-
-	// Mise en attente d'un signal envoye par le controleur pour reprendre
-	pthread_mutex_lock( args->mutex );
-	pthread_cond_wait( args->varCond, args->mutex );
-	pthread_mutex_unlock( args->mutex );
-      }
-    }
 }
