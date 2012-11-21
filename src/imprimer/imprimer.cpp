6a367f5a0557c15e4a5d9f4efd055552fbc7cf71
Renaud <renaud@renaud-1015PEM.(none)>
2012-11-21 08:08:49 +0100

    Changes imprimer

--- src/imprimer/imprimer.cpp
+++ src/imprimer/imprimer.cpp
@@ -34,58 +34,50 @@ void ecriture_log_imprimer(Log * unGestionnaire, std::string msg,logType unType)
 //---------------------------------------------------- Fonctions publiques
 int imprimer_thread(void * argsUnconverted)
 {
-  ArgImprimer * args= (ArgImprimer *)argsUnconverted;
-  ecriture_log_imprimer(args->gestionnaireLog,"Lancement de la tache imprimer",ERROR);	
+	ArgImprimer * args= (ArgImprimer *)argsUnconverted;
+	ecriture_log_imprimer(args->gestionnaireLog,"Lancement de la tache imprimer",ERROR);	
 
-  pthread_cond_t varCond;
-  pthread_mutex_t mutex;
+	pthread_cond_t varCond;
+	pthread_mutex_t mutex;
 
-  for (;;) {
-    ecriture_log_imprimer(args->gestionnaireLog,"Tache imprimer va puller",ERROR);
-    Carton cartonImpression = args->balImprimante->Pull();
-    ecriture_log_imprimer(args->gestionnaireLog,"Carton arrivee - tache imprimer",ERROR);
-    if(cartonImpression.fin==true)
-      {
-	ecriture_log_imprimer(args->gestionnaireLog,"Fin de la tache imprimer",ERROR);
-	Carton c;
-	c.fin=true;
-	args->balPalette->Push(c,0);
-	pthread_exit(0);
-      }
+	for (;;) {
+		ecriture_log_imprimer(args->gestionnaireLog,"Tache imprimer va puller",ERROR);
+		Carton cartonImpression = args->balImprimante->Pull();
+		ecriture_log_imprimer(args->gestionnaireLog,"Carton arrivee - tache imprimer",ERROR);
+		if(cartonImpression.fin==true) {
+			ecriture_log_imprimer(args->gestionnaireLog,"Fin de la tache imprimer",ERROR);
+			Carton c;
+			c.fin=true;
+			args->balPalette->Push(c,0);
+			pthread_exit(0);
+		}
 
-    if (!stubPanneImprimante()) {
-
-      if (args->balPalette->Size() < 10) {
-
-	// Depot Carton dans la bal balPalette
-	args->balPalette->Push(cartonImpression,0);
-
-      }
-      else {
-
-	// Depot message erreur dans la bal enventbox
-	ecriture_log_imprimer(args->gestionnaireLog,"File d'attente pleine - tache imprimer",ERROR);
-	Event msgErreurFileAttente(FILEATTPLEINE);
-	args->eventBox->Push( msgErreurFileAttente,0 );
-				
-	// Mise en attente d'un signal envoye par le controleur pour reprendre
-	pthread_mutex_lock( args->mutex );
-	pthread_cond_wait( args->varCond, args->mutex );
-	pthread_mutex_unlock( args->mutex );
-
-      }
-
-    }
-    else {
-      ecriture_log_imprimer(args->gestionnaireLog,"Panne imprimante - tache imprimer",ERROR);
-      // Depot message erreur dans la bal eventbox
-      Event msgErreurImp(PANNEIMPRIM);
-      args->eventBox->Push( msgErreurImp ,0);
+		if (stubPanneImprimante()) {
+			ecriture_log_imprimer(args->gestionnaireLog,"Panne imprimante - tache imprimer",ERROR);
+			// Depot message erreur dans la bal eventbox
+			Event msgErreurImp(PANNEIMPRIM);
+			args->eventBox->Push( msgErreurImp ,0);
 		
-      // Mise en attente d'un signal envoye par le controleur pour reprendre
-      pthread_mutex_lock( args->mutex );
-      pthread_cond_wait( args->varCond, args->mutex );
-      pthread_mutex_unlock( args->mutex );
-    }
-  }
+			// Mise en attente d'un signal envoye par le controleur pour reprendre
+			pthread_mutex_lock( args->mutex );
+			pthread_cond_wait( args->varCond, args->mutex );
+			pthread_mutex_unlock( args->mutex );
+		}
+
+		if (args->balPalette->Size() >= 10) {
+
+			// Depot message erreur dans la bal enventbox
+			ecriture_log_imprimer(args->gestionnaireLog,"File d'attente pleine - tache imprimer",ERROR);
+			Event msgErreurFileAttente(FILEATTPLEINE);
+			args->eventBox->Push( msgErreurFileAttente,0 );
+				
+			// Mise en attente d'un signal envoye par le controleur pour reprendre
+			pthread_mutex_lock( args->mutex );
+			pthread_cond_wait( args->varCond, args->mutex );
+			pthread_mutex_unlock( args->mutex );
+		}
+
+		// Depot Carton dans la bal balPalette
+		args->balPalette->Push(cartonImpression,0);
+	}
 }
--- tests/imprimer/testTacheImprimer.cpp
+++ tests/imprimer/testTacheImprimer.cpp
@@ -8,6 +8,7 @@
 #include "../../src/log/log.h"
 
 #define MAXCARTON 15
+#define MAXCARTONPANNE 1000
 
 using namespace std;
 
@@ -20,12 +21,60 @@ static pthread_cond_t varCond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 //Fontions pour tests
-void ecriture_log_mere(Log * unGestionnaire, std::string msg,logType unType);                                                              
-void depotCartonBalImprimante(ArgImprimer * args, int numCarton);
-void retraitCartonBalPalette(ArgImprimer * args);
-void simuFonctionnementNormal(ArgImprimer * args);
-void simuFilePleine (ArgImprimer * args);
-void viderFilePleine(ArgImprimer * args);
+void ecriture_log_mere(Log * unGestionnaire, std::string msg,logType unType)                                                                                     
+{
+  #ifdef DEBUG
+    unGestionnaire->Write(msg,unType,true);
+  #else
+    unGestionnaire->Write(msg,unType,false);
+  #endif 
+}
+
+void depotCartonBalImprimante(ArgImprimer * args, int numCarton, int maxCarton) {
+	Carton carton;
+	carton.id = numCarton;
+	carton.lot = NULL;
+	carton.nbrebut = 0;
+	if (numCarton == maxCarton)
+		carton.fin = true;
+	else
+		carton.fin = false;
+	args->balImprimante->Push(carton, 1);
+}
+
+void retraitCartonBalPalette(ArgImprimer * args) {
+	Carton carton;
+	carton = args->balPalette->Pull();
+} 
+
+void simuFonctionnementNormal(ArgImprimer * args) {
+	ecriture_log_mere(&gestionnaireLog,"SIMULATION FONCTIONNEMENT NORMAL",EVENT);
+	for (int i=1; i<=MAXCARTON; i++) {
+		depotCartonBalImprimante( args, i, MAXCARTON );
+		retraitCartonBalPalette(args);
+	}
+}
+
+void simuFilePleine (ArgImprimer * args) {
+	ecriture_log_mere(&gestionnaireLog,"SIMULATION FILE PLEINE",EVENT);
+	for (int i=1; i<=MAXCARTON; i++) {
+		depotCartonBalImprimante( args, i, MAXCARTON );
+	}
+}
+ void viderFilePleine(ArgImprimer * args) {
+	for (int i=1; i<=10; i++) {
+		retraitCartonBalPalette( args );
+	}
+	pthread_cond_signal(&varCond);
+}
+
+void simuPanneImprim(ArgImprimer * args) {
+	ecriture_log_mere(&gestionnaireLog,"SIMULATION PANNE IMPRIMANTE",EVENT);
+	for (int i=1; i<=MAXCARTONPANNE; i++) {
+		depotCartonBalImprimante( args, i, MAXCARTONPANNE );
+		retraitCartonBalPalette(args);
+	}
+}
 
 int main() {
 
@@ -47,64 +96,33 @@ int main() {
 	pthread_create (&imprimer, NULL,(void *(*) (void *)) &imprimer_thread, argImprimer);
 	
 	ecriture_log_mere(&gestionnaireLog,"Phase moteur - tache mere",EVENT);
+	
+	int choix;
+	cout << "Choix du test : " << endl;
+	cout << "Test 1 : Fonctionnement normal\n"
+		<< "Test 2 : Remplissage de la file d'attente puis vidage avec reprise de la tache\n"
+		<< "Test 3 : Simulation d'une panne imprimante\n" << "Votre choix :" << endl;
+	cin >> choix;
 
-	//simuFonctionnementNormal(argImprimer);	
-
-	simuFilePleine(argImprimer);
-	sleep(1);
-	viderFilePleine(argImprimer);
-
+	if (choix==1) {
+		//Test 1 : Fonctionnement normal
+		simuFonctionnementNormal(argImprimer);
+	}	
+	else if (choix==2) {
+		//Test 2 : Remplissage de la file d'attente puis vidage avec reprise de la tache
+		simuFilePleine(argImprimer);
+		sleep(1);
+		viderFilePleine(argImprimer);
+	}
+	else if (choix==3) {
+		//Test 3 : Simulation d'une panne imprimante
+		simuPanneImprim(argImprimer);
+	}
+	 
 	pthread_join(imprimer, NULL);
 	
 	ecriture_log_mere(&gestionnaireLog,"Fin de la tâche mère",EVENT);
 
 	return 0;
 
-}
-
-void ecriture_log_mere(Log * unGestionnaire, std::string msg,logType unType)                                                                                     
-{
-  #ifdef DEBUG
-    unGestionnaire->Write(msg,unType,true);
-  #else
-    unGestionnaire->Write(msg,unType,false);
-  #endif 
-}
-
-void depotCartonBalImprimante(ArgImprimer * args, int numCarton) {
-	Carton carton;
-	carton.id = numCarton;
-	carton.lot = NULL;
-	carton.nbrebut = 0;
-	if (numCarton == MAXCARTON)
-		carton.fin = true;
-	else
-		carton.fin = false;
-	args->balImprimante->Push(carton, 1);
-}
-
-void retraitCartonBalPalette(ArgImprimer * args) {
-	Carton carton;
-	carton = args->balPalette->Pull();
-} 
-
-void simuFonctionnementNormal(ArgImprimer * args) {
-	ecriture_log_mere(&gestionnaireLog,"SIMULATION FONCTIONNEMENT NORMAL",EVENT);
-	for (int i=1; i<=MAXCARTON; i++) {
-		depotCartonBalImprimante( args, i );
-		retraitCartonBalPalette(args);
-	}
-}
-
-void simuFilePleine (ArgImprimer * args) {
-	ecriture_log_mere(&gestionnaireLog,"SIMULATION FILE PLEINE",EVENT);
-	for (int i=1; i<=MAXCARTON; i++) {
-		depotCartonBalImprimante( args, i );
-	}
-}
- void viderFilePleine(ArgImprimer * args) {
-	for (int i=1; i<=10; i++) {
-		retraitCartonBalPalette( args );
-	}
-	pthread_cond_signal(&varCond);
 }
